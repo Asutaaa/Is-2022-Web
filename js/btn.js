ff2704eb56294a87085489c53ed137b8cbe4ae36
Asutaaa <79144388+Asutaaa@users.noreply.github.com>
2022-12-18 18:16:08 +0300

    fixed: btn.js and table.js

--- js/btn.js
+++ js/btn.js
@@ -1,12 +1,21 @@
 document.addEventListener('click', function(evt){
-  evt.preventDefault;
   if(evt.target.matches('.btn__add')){
+    evt.preventDefault();
+    add__form.className = 'plan_add_form_yes';
     let el = evt.target;
-    let text = prompt('task', 'Ещё одна задача');
-    let cont = el.parentElement;
-    task = '<p class = "plan__content">'+text+'</p>';
-    cont.innerHTML += '<div class = "plan__allContent">' + task + '<button class="btn__delete">-</button></div>';
-}
+    let cont = el.parentElement.parentElement;
+    let add__plan = document.querySelector('#promt_input');
+    add__plan.addEventListener('submit', function(evt_sub){
+      evt_sub.preventDefault();
+      let data = new FormData(add__plan);
+      console.log(data.get('add_plan'));
+      let newNode = document.importNode(plan_template.content, true);
+      newNode.querySelector('p.plan__content').textContent = data.get('add_plan');
+      cont.appendChild(newNode);
+      newNode = null;
+      add__form.className = 'plan_add_form_no';
+    }, {once:true, passive:false,});
+  }
   if(evt.target.matches('.btn__delete')){
     evt.target.parentElement.remove();
   }
--- js/table.js
+++ js/table.js
@@ -2,18 +2,23 @@ const myForm = document.querySelector('#studyplan');
 myForm.addEventListener('submit', function(evt){
   evt.preventDefault();
   let data = new FormData(myForm);
-  const wrap = document.querySelector('main');
-  let htmlContent = '<grid id="table" class="plan">';
-  htmlContent
+  const wrap = document.querySelector('._todoList');
+  document.querySelector('.hello').innerHTML += `<h3>Привет, `+data.get('name')+`.</h3>`;
+  wrap.innerHTML = '<grid id="table" class="plan"></grid>';
   let gtc = '';
   for(let i = 0; i < data.get('day'); i++){
-    htmlContent += `<div class = "plan__name">` + (i+1) + ` Mounth<button class="btn__add">+</button></div>`;
+    let newNode = document.importNode(mounth.content, true);
+    newNode.querySelector('p.plan__name_title_content').textContent = (i + 1) + ' Mounth';
+    table.appendChild(newNode);
     gtc += '1fr ';
   }
-  htmlContent += '</grid>'
-  wrap.innerHTML = htmlContent;
+  let addForm = document.importNode(plan_add.content, true);
+  wrap.appendChild(addForm)
   document.querySelector('#table').style.gridTemplateColumns = gtc;
   wrap.classList.add('todoList')
+  localStorage.setItem('name', data.get('name'));
+  localStorage.setItem('day', data.get('day'));
+  localStorage.setItem('language', data.get('language'));
   const contentJava = [['Освоить синтаксис языка Java','Научиться писать понятный код','Освоить понятия SQl'],['написать маленькие Java приложения','Шаблоны проектирования','Читать логи, дебажить'],['Разработка маленького Spring приложения','Подключение SQL','Изучить запросы']];
   const contentC = [[' Введение в С#','Основные понятия ООП','Наследование и полиморфизм'],['Интерфейсы','Делегаты и события','Управление памятью и указатели '],['Потоки','LINQ to Objects','Рефлексия']];
   const arr = document.querySelectorAll('.plan__name');
@@ -21,14 +26,16 @@ myForm.addEventListener('submit', function(evt){
     let text = ''
     if (data.get('language')=='Java'){
       for (let k = 0; k < 3; k++){
-        text = '<p class = "plan__content">'+contentJava[i][k]+'</p>';
-        arr[i].innerHTML += `<div class = "plan__allContent">`+text+`<button class="btn__delete">-</button></div>`;
+        let newNode = document.importNode(plan_template.content, true);
+        newNode.querySelector('p.plan__content').textContent = contentJava[i][k];
+        arr[i].appendChild(newNode);
       }
   }
     else{
       for (let k = 0; k < 3; k++){
-        text = '<p class = "plan__content">'+contentC[i][k]+'</p>';
-        arr[i].innerHTML += `<div class = "plan__allContent">`+text+`<button class="btn__delete">-</button></div>`;
+        let newNode = document.importNode(plan_template.content, true);
+        newNode.querySelector('p.plan__content').textContent = contentC[i][k];
+        arr[i].appendChild(newNode);
       }
   }
 }}
